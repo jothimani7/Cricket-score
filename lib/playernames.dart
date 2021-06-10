1cec206d63e166fa9e9507dd5a118b8fae43006a
jothimani7 <boopathiarts2017@gmail.com>
2021-06-10 20:37:21 +0530

    first commit

--- lib/playernames.dart
+++ lib/playernames.dart
@@ -1,7 +1,9 @@
 
 
+// import 'dart:developer';
 
 import 'package:flutter/material.dart';
+// import 'dart:convert';
 
 class PlayersName extends StatefulWidget {
   // const PlayersName({Key key}) : super(key: key);
@@ -36,7 +38,7 @@ class _PlayersNameState extends State<PlayersName> {
                   itemBuilder: (context,index) => PlayerCard(
                     title: playersList[index].title,
                     subtitle: playersList[index].subtitle,
-                  )
+                  ),
               ),
             ),
             ElevatedButton(
@@ -55,12 +57,14 @@ class _PlayersNameState extends State<PlayersName> {
   void _navigateAndDisplay(BuildContext context) async{
     // _PlayersNameState obj = new _PlayersNameState();
     final result = await Navigator.of(context).push(MaterialPageRoute(builder: (context)=> AddPlayer()));
+    if(result!=null)
     playersList.add(result);
+    // print(result==null);
     setState(() {
 
     });
-    // var names = playersList.map((players) => players.title).toList();
-    // print(names);
+    var names = playersList.map((players) => players.title).toList();
+    print(names);
   }
 
 }
@@ -77,39 +81,94 @@ class _AddPlayerState extends State<AddPlayer> {
   // _PlayersNameState obj = new _PlayersNameState();
   Players obj = new Players();
   TextEditingController name = new TextEditingController();
-  TextEditingController field = new TextEditingController();
-
+  // TextEditingController field = new TextEditingController();
+  String field;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('AddPlayer'),
       ),
+      backgroundColor: Colors.black12,
       body: Padding(
         padding: const EdgeInsets.all(10.0),
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              TextField(
-                controller: name,
-                decoration: InputDecoration(
-                  labelText: 'Enter Player Name',
+              Container(
+                width: 250,
+                child: TextField(
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                  controller: name,
+                  decoration: InputDecoration(
+                    enabledBorder: const OutlineInputBorder(
+                      borderSide: const BorderSide(color: Colors.white),
+                    ),
+                    border: OutlineInputBorder(),
+                    labelText: 'Enter Player name',
+                    labelStyle: TextStyle(
+                        color: Colors.white54,
+                    ),
+                  ),
                 ),
               ),
-              TextField(
-                controller: field,
-                decoration: InputDecoration(
-                  labelText: 'Enter Player Field',
+              SizedBox(height: 30,),
+              Container(
+                padding: EdgeInsets.all(10),
+                width: 250,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10)
+                ),
+                child: DropdownButtonHideUnderline(
+                  child: DropdownButton(
+                    dropdownColor: Colors.blueGrey,
+                    // focusColor: Colors.blue,
+                    icon: Icon(Icons.arrow_drop_down_circle),
+                    underline: SizedBox(),
+                    hint: Text('Choose Field'),
+                    value: field,
+                    onChanged: (newValue){
+                      setState(() {
+                        field = newValue;
+                      });
+                    },
+                    items: ['Batsman','Bowler','All-Rounder'].map((ovr){
+                      return DropdownMenuItem(
+                        child: new Text(ovr),
+                        value: ovr,
+                      );
+                    }).toList(),
+                  ),
                 ),
               ),
-              ElevatedButton(
-                child: Text('Add palyer'),
-                onPressed: (){
-                  // obj.title=name.text;
-                  // obj.subtitle=field.text;
-                  Navigator.pop(context,Players(title: name.text, subtitle: field.text));
-                },
+              SizedBox(height: 30,),
+              ConstrainedBox(
+                constraints: BoxConstraints.tightFor(height: 50,width: 150),
+                child: ElevatedButton(
+                  child: Text('Add palyer',style: TextStyle(
+                    fontSize: 18,
+                  ),),
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18)
+                      )
+                    ),
+                  ),
+                  onPressed: (){
+                    // obj.title=name.text;
+                    // obj.subtitle=field.text;
+                    if(name.text!=''&&field!='')
+                    Navigator.pop(context,Players(title: name.text, subtitle: field));
+                    else {
+                      Navigator.pop(context, null);
+                    }
+                  },
+                ),
               )
             ],
           ),
